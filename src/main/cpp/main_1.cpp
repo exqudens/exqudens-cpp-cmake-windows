e708f3bd1d96edb91ccbb8f8b5019e3b95550850
exqudens <exqudens@gmail.com>
2021-02-20 00:18:01 -0800

    step 4

--- src/main/cpp/main_1.cpp
+++ src/main/cpp/main_1.cpp
@@ -20,7 +20,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
   cout << "0: " << p.string() << endl;
   cout << "1: " << hInstance << endl;
-  cout << "2: " << hInstance->i << endl;
+  //cout << "2: " << hInstance->i << endl;
   cout << "3: " << hPrevInstance << endl;
   //cout << "4: " << hPrevInstance->i << endl;
   cout << "5: " << lpCmdLine << endl;
